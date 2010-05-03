e49462163232f8510779013960588d91d011da10
Sebastian Wiesner <lunaryorn@googlemail.com>
2010-05-03 22:03:45 +0200

    Fixed modules listing in setup file

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(
         'Topic :: System :: Hardware',
         'Topic :: System :: Operating System Kernels :: Linux',
         ],
-    modules=['udev', '_udev'],
+    py_modules=['udev', '_udev'],
     )
