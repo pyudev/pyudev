80e7f51cb1788d81fe6659e3b5838096089504c9
Sebastian Wiesner <lunaryorn@googlemail.com>
2010-08-24 09:55:13 +0200

    Update development state

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     platforms='Linux',
     license='MIT/X11',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
