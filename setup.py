cc25d64d1cf2e63940bcc2cca7def8124e66e13c
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-08-30 23:34:12 +0200

    Do not fix imports for python, addresses #33
    2to3 "import" fixers blews up absolute imports by changing them back to
    relative imports in some python versions, see #8358.  Affects the "pyudev.glib"
    module, which uses an absolute import for "glib".  Applying the "import" fixer
    to this module breaks pyudev.glib for python 3.

--- setup.py
+++ setup.py
@@ -18,27 +18,45 @@
 
 
 import sys
+try:
+    import setuptools
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    import setuptools
+
+from setuptools.command.build_py import build_py
+import setuptools.command.build_py
+
+import pyudev
+
+def find_fixers(blacklist=None):
+    blacklist = blacklist or []
+    names = getattr(build_py, 'fixer_names', None) or []
+    from lib2to3.refactor import get_fixers_from_package
+    for p in setuptools.lib2to3_fixer_packages:
+        names.extend(get_fixers_from_package(p))
+    # explicitly remove all blacklisted fixers to trigger value errors on
+    # non-existing filters in the blacklist
+    for f in blacklist:
+        names.remove(f)
+    build_py.fixer_names = names
+
 if sys.version_info[0] < 3:
     from codecs import open
     extra_arguments = {}
 else:
+    # Remove import fixer, because it blews up absolute imports on some python
+    # versions, see Python bug #8358
+    find_fixers(blacklist=['lib2to3.fixes.fix_import'])
     extra_arguments = dict(use_2to3=True)
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    import distribute_setup
-    distribute_setup.use_setuptools()
-    from setuptools import setup, find_packages
-
-import pyudev
 
 
 with open('README.rst', encoding='utf-8') as stream:
     long_description = stream.read()
 
 
-setup(
+setuptools.setup(
     name='pyudev',
     version=str(pyudev.__version__),
     url='http://packages.python.org/pyudev',
@@ -60,6 +78,6 @@ setup(
         'Topic :: System :: Hardware',
         'Topic :: System :: Operating System Kernels :: Linux',
         ],
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     **extra_arguments
     )
