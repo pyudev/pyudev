9db0236007550ddc57cd00c4c5482442413613c4
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-08-30 22:51:00 +0200

    Fixed import check for pygobject

--- tests/bindings/pygobject.mk
+++ tests/bindings/pygobject.mk
@@ -1,7 +1,7 @@
 gnomedownloads = http://ftp.gnome.org/pub/GNOME/sources
 pygobject = pygobject-2.28.6
 pygobjecturl = $(gnomedownloads)/pygobject/2.28/$(pygobject).tar.bz2
-havegobject = $(and $(call checkmodule,glib),$(call checkmodule,gobject))
+havepygobject = $(and $(call checkmodule,glib),$(call checkmodule,gobject))
 
 $(eval $(call download-rule,$(pygobjecturl),$(pygobject).tar.bz2))
 $(eval $(call builddir-rule,$(pygobject).tar.bz2,$(pygobject)))
