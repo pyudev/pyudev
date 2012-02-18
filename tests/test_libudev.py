05ddb5536beab9412680f5a0731e0c0503d7d18e
Sebastian Wiesner <lunaryorn@googlemail.com>
2012-02-18 22:35:44 +0100

    Turned binding coverage into unit tests
    This test currently fails because the blacklist is incomplete.  Subsequent work
    needs to complete the bindings and/or blacklist additional functions

--- tests/test_libudev.py
+++ tests/test_libudev.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2011 Sebastian Wiesner <lunaryorn@googlemail.com>
+# Copyright (C) 2011, 2012 Sebastian Wiesner <lunaryorn@googlemail.com>
 
 # This library is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by the
@@ -18,6 +18,11 @@
 
 from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
+
+import re
+from subprocess import check_call
+from tempfile import NamedTemporaryFile
+from xml.etree import ElementTree as etree
 
 import pytest
 
@@ -41,6 +46,17 @@ UDEV_ADDITIONS = {
     }
 
 
+LIBUDEV_H = '/usr/include/libudev.h'
+
+
+def get_libudev_functions():
+    with NamedTemporaryFile() as stream:
+        check_call(['gccxml', LIBUDEV_H, '-fxml={0}'.format(stream.name)])
+        tree = etree.parse(stream.name)
+    names = [f.get('name') for f in tree.findall('//Function')]
+    return [n for n in names if n.startswith('udev_')]
+
+
 def pytest_generate_tests(metafunc):
     if 'funcname' in metafunc.funcargnames:
         for namespace, members in binding.SIGNATURES.items():
@@ -54,6 +70,9 @@ def pytest_generate_tests(metafunc):
                 funcargs = dict(version=version, missing_function=function)
                 testid = 'version {0}, {1}'.format(version, function)
                 metafunc.addcall(funcargs=funcargs, id=testid)
+    elif metafunc.function.__name__ == 'test_is_wrapped':
+        for function in get_libudev_functions():
+            metafunc.addcall(funcargs=dict(function_name=function), id=function)
 
 
 def pytest_funcarg__signature(request):
@@ -98,3 +117,42 @@ def test_missing_functions(version, missing_function):
         pytest.skip('{0} already exists in {1}'.format(
             missing_function, pytest.config.udev_version))
     assert not hasattr(libudev, missing_function)
+
+
+WRAPPER_BLACKLIST_PATTERNS = [
+     # vararg functions not supported by ctypes
+    'udev_set_log_fn',
+    # superfluous in Python, because arbitrary attributes can be attached to
+    # objects anyways
+    'udev_set_userdata', 'udev_get_userdata',
+    # superfluous, because context is already available in .context
+    'udev_enumerate_get_udev', 'udev_monitor_get_udev', 'udev_device_get_udev',
+    # superfluous, because Python provides already tools to filter lists
+    'udev_list_entry_get_by_name',
+    # undocumented in libudev manual
+    'udev_device_get_seqnum',
+    # all queue functions (queue interface is not wrapped)
+    re.compile('^udev_queue_.*')
+]
+
+
+def _is_blacklisted(function_name):
+    for pattern in WRAPPER_BLACKLIST_PATTERNS:
+        if pytest.is_unicode_string(pattern):
+            if function_name == pattern:
+                return True
+        else:
+            if pattern.match(function_name):
+                return True
+    else:
+        return False
+
+
+def test_is_wrapped(function_name):
+    wrapped_functions = set('{0}_{1}'.format(ns, member)
+                            for ns, members in binding.SIGNATURES.items()
+                            for member in members)
+    if _is_blacklisted(function_name):
+        assert function_name not in wrapped_functions
+    else:
+        assert function_name in wrapped_functions
