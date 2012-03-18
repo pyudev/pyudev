c60787a5b05f1b53c7b25af38c14a9e91a1894ee
Sebastian Wiesner <lunaryorn@googlemail.com>
2012-03-18 14:06:52 +0100

    Refactored observer tests
    Use xUnit style test classes with abstract methods instead of this somewhat
    hacky and messy binding class

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -18,13 +18,12 @@
 from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
 
-from itertools import count
 from functools import partial
 
 import pytest
 from mock import Mock
 
-from pyudev import Monitor
+from pyudev import Monitor, Device
 
 
 def pytest_funcarg__monitor(request):
@@ -32,209 +31,8 @@ def pytest_funcarg__monitor(request):
 
 
 def pytest_funcarg__fake_monitor_device(request):
-    from pyudev import Device
     context = request.getfuncargvalue('context')
     return Device.from_path(context, '/devices/platform')
-
-
-class BaseBinding(object):
-    def trigger_observer(self, action, monitor, action_trigger):
-        mainloop = self.create_mainloop()
-
-        signal_counter = count(1)
-
-        def _quit_when_done(*args, **kwargs):
-            value = next(signal_counter)
-            if value >= 2:
-                self.quit_mainloop(mainloop)
-
-        # slot dummies
-        event_slot = Mock(side_effect=_quit_when_done)
-        action_slots = {action: Mock(side_effect=_quit_when_done)}
-        observer = self.create_observer(monitor)
-        self.connect_signals(observer, event_slot, action_slots)
-
-        self.single_shot(0, action_trigger)
-        self.single_shot(5000, partial(self.quit_mainloop, mainloop))
-        self.run_mainloop(mainloop)
-        self.cleanup(observer)
-        return event_slot, action_slots
-
-
-class Qt4Binding(BaseBinding):
-
-    def __init__(self, bindingname):
-        self.name = bindingname
-
-    def create_observer(self, monitor):
-        name = self.name.lower()
-        mod = __import__('pyudev.{0}'.format(name), None, None, [name])
-        observer = mod.QUDevMonitorObserver(monitor)
-        assert observer.monitor is monitor
-        assert isinstance(observer.notifier, self.qtcore.QSocketNotifier)
-        return observer
-
-    def import_or_skip(self):
-        self.qtcore = pytest.importorskip('{0}.QtCore'.format(self.name))
-
-    def create_mainloop(self):
-        app = self.qtcore.QCoreApplication.instance()
-        if not app:
-            app = self.qtcore.QCoreApplication([])
-        return app
-
-    def quit_mainloop(self, app):
-        app.quit()
-
-    def run_mainloop(self, app):
-        app.exec_()
-
-    def connect_signals(self, observer, event_slot, action_slots):
-        observer.deviceEvent.connect(event_slot)
-        signal_map = {'add': observer.deviceAdded,
-                      'remove': observer.deviceRemoved,
-                      'change': observer.deviceChanged,
-                      'move': observer.deviceMoved}
-        for action, slot in action_slots.items():
-            signal_map[action].connect(slot)
-
-    def single_shot(self, timeout, callback):
-        self.qtcore.QTimer.singleShot(timeout, callback)
-
-    def cleanup(self, observer):
-        observer.enabled = False
-
-
-class GlibBinding(BaseBinding):
-    name = 'Glib'
-
-    def __init__(self):
-        self.event_sources = []
-
-    def create_observer(self, monitor):
-        from pyudev.glib import GUDevMonitorObserver
-        observer = GUDevMonitorObserver(monitor)
-        assert observer.monitor is monitor
-        assert isinstance(observer.event_source, int)
-        return observer
-
-    def import_or_skip(self):
-        self.glib = pytest.importorskip('glib')
-        pytest.importorskip('gobject')
-
-    def create_mainloop(self):
-        return self.glib.MainLoop()
-
-    def quit_mainloop(self, mainloop):
-        mainloop.quit()
-
-    def run_mainloop(self, mainloop):
-        mainloop.run()
-
-    def wrap_callback(self, callback):
-        """
-        Remove the emitter from callback invocation, it is not of any
-        interest to the test code.
-        """
-        def _wrapper(obj, *args, **kwargs):
-            return callback(*args, **kwargs)
-        return _wrapper
-
-    def connect_signals(self, observer, event_slot, action_slots):
-        observer.connect('device-event', self.wrap_callback(event_slot))
-        signal_map = {'add': 'device-added', 'remove': 'device-removed',
-                      'change': 'device-changed', 'move': 'device-moved'}
-        for action, slot in action_slots.items():
-            observer.connect(signal_map[action], self.wrap_callback(slot))
-
-    def single_shot(self, timeout, callback):
-        def _callback():
-            callback()
-            return False
-        self.event_sources.append(self.glib.timeout_add(timeout, _callback))
-
-    def cleanup(self, observer):
-        observer.enabled = False
-        # cleanup all timer event sources.  If this was not done, all tests
-        # save the first would spin endlessly.  Apparently older event sources
-        # kind of "confuse" the main loop of the current test
-        for source in self.event_sources:
-            self.glib.source_remove(source)
-        self.event_sources = []
-
-
-class WXBinding(BaseBinding):
-    name = 'wx'
-
-    timer = []
-
-    def create_observer(self, monitor):
-        from pyudev.wx import WxUDevMonitorObserver
-        observer = WxUDevMonitorObserver(monitor)
-        assert observer.monitor is monitor
-        return observer
-
-    def import_or_skip(self):
-        self.wx = pytest.importorskip('wx')
-
-    def create_mainloop(self):
-        app = self.wx.App(False)
-        app.frame = self.wx.Frame(None)
-        self.frame = app.frame
-        return app
-
-    def quit_mainloop(self, app):
-        app.frame.Close()
-
-    def run_mainloop(self, app):
-        app.MainLoop()
-
-    def connect_signals(self, observer, event_slot, action_slots):
-        def wrap_callback(callback):
-            def _wrapper(event):
-                if (hasattr(event, 'action')):
-                    return callback(event.action, event.device)
-                else:
-                    return callback(event.device)
-            return _wrapper
-
-        import pyudev.wx
-        observer.Bind(pyudev.wx.EVT_DEVICE_EVENT, wrap_callback(event_slot))
-        event_map = {
-                'add': pyudev.wx.EVT_DEVICE_ADDED,
-                'remove': pyudev.wx.EVT_DEVICE_REMOVED,
-                'change': pyudev.wx.EVT_DEVICE_CHANGED,
-                'move': pyudev.wx.EVT_DEVICE_MOVED }
-        for action, slot in action_slots.items():
-            observer.Bind(event_map[action], wrap_callback(slot))
-
-    def single_shot(self, timeout, callback):
-        timer = self.wx.PyTimer(callback)
-        self.timer.append(timer)
-        timer.Start(timeout, True)
-
-    def cleanup(self, observer):
-        # disable the observer to avoid that some pending observers confuse our
-        # tests
-        observer.enabled = False
-
-
-BINDINGS = [Qt4Binding('PyQt4'), Qt4Binding('PySide'), GlibBinding(), WXBinding()]
-ACTIONS = ('add', 'remove', 'change', 'move')
-
-
-def pytest_generate_tests(metafunc):
-    if 'binding' in metafunc.funcargnames:
-        for binding in BINDINGS:
-            funcargs = dict(binding=binding)
-            id = binding.name
-            if 'action' in metafunc.funcargnames:
-                for action in ACTIONS:
-                    funcargs.update(action=action)
-                    metafunc.addcall(funcargs=funcargs,
-                                     id=id + ',' + action)
-            else:
-                metafunc.addcall(funcargs=funcargs, id=id)
 
 
 def test_fake_monitor(fake_monitor, fake_monitor_device):
@@ -248,32 +46,239 @@ def test_fake_monitor(fake_monitor, fake_monitor_device):
         assert device == fake_monitor_device
 
 
-class TestObserver(object):
+ACTIONS = ('add', 'remove', 'change', 'move')
 
-    def test_observer_fake(self, binding, action, fake_monitor,
-                           fake_monitor_device):
-        binding.import_or_skip()
-        event_slot, action_slots = binding.trigger_observer(
-            action, fake_monitor, lambda: fake_monitor.trigger_action(action))
-        # check, that both slots were called
-        event_slot.assert_called_with(action, fake_monitor_device)
-        action_slots.pop(action).assert_called_with(fake_monitor_device)
+
+class ObserverTestBase(object):
+
+    def setup_method(self, method):
+        self.observer = None
+        self.no_emitted_signals = 0
+        self.setup()
+
+    def teardown_method(self, method):
+        if self.observer is not None:
+            self.destroy_observer()
+        self.teardown()
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def destroy_observer(self):
+        self.observer.enabled = False
+
+    def create_observer(self, monitor):
+        raise NotImplementedError()
+
+    def create_event_loop(self, self_stop_timeout=5000):
+        raise NotImplementedError()
+
+    def start_event_loop(self, start_callback):
+        raise NotImplementedError()
+
+    def stop_event_loop(self):
+        raise NotImplementedError()
+
+    def connect_signal(self, callback, action=None):
+        raise NotImplementedError()
+
+    def stop_when_done(self, *args, **kwargs):
+        self.no_emitted_signals += 1
+        if self.no_emitted_signals >= 2:
+            self.stop_event_loop()
+
+    def prepare_test(self, monitor):
+        self.create_event_loop(self_stop_timeout=5000)
+        self.create_observer(monitor)
+
+    def test_monitor(self, fake_monitor):
+        self.prepare_test(fake_monitor)
+        # test that the monitor attribute is correct
+        assert self.observer.monitor is fake_monitor
+
+    @pytest.mark.parametrize('action', ACTIONS, ids=ACTIONS)
+    def test_events_fake_monitor(self, action, fake_monitor, fake_monitor_device):
+        self.prepare_test(fake_monitor)
+        event_callback = Mock(side_effect=self.stop_when_done)
+        action_callback = Mock(side_effect=self.stop_when_done)
+        self.connect_signal(event_callback)
+        self.connect_signal(action_callback, action=action)
+        self.start_event_loop(partial(fake_monitor.trigger_action, action))
+        event_callback.assert_called_with(action, fake_monitor_device)
+        action_callback.assert_called_with(fake_monitor_device)
 
     @pytest.mark.privileged
-    def test_observer(self, binding, monitor):
-        binding.import_or_skip()
+    def test_events_real(self, context, monitor):
+        # make sure that the module is unloaded initially
         pytest.unload_dummy()
         monitor.filter_by('net')
         monitor.enable_receiving()
-        event_slot, action_slot = binding.trigger_observer(
-            'add', monitor, pytest.load_dummy)
-        action, device = event_slot.call_args[0]
-        assert action == 'add'
-        assert device.subsystem == 'net'
-        assert device.device_path == '/devices/virtual/net/dummy0'
-        event_slot, action_slot = binding.trigger_observer(
-            'remove', monitor, pytest.unload_dummy)
-        action, device = event_slot.call_args[0]
-        assert action == 'remove'
-        assert device.subsystem == 'net'
-        assert device.device_path == '/devices/virtual/net/dummy0'
+        self.prepare_test(monitor)
+        # setup signal handlers
+        event_callback = Mock(side_effect=self.stop_when_done)
+        added_callback = Mock(side_effect=self.stop_when_done)
+        removed_callback = Mock(side_effect=self.stop_when_done)
+        self.connect_signal(event_callback)
+        self.connect_signal(added_callback, action='add')
+        self.connect_signal(removed_callback, action='remove')
+
+        # test add event
+        self.start_event_loop(pytest.load_dummy)
+        device = Device.from_path(context, '/devices/virtual/net/dummy0')
+        event_callback.assert_called_with('add', device)
+        added_callback.assert_called_with(device)
+        assert not removed_callback.called
+
+        for mock in (event_callback, added_callback, removed_callback):
+            mock.reset_mock()
+
+        self.start_event_loop(pytest.unload_dummy)
+        event_callback.assert_called_with('remove', device)
+        assert not added_callback.called
+        removed_callback.assert_called_with(device)
+
+
+class QtObserverTestBase(ObserverTestBase):
+
+    ACTION_SIGNAL_MAP = {
+        'add': 'deviceAdded',
+        'remove': 'deviceRemoved',
+        'change': 'deviceChanged',
+        'move': 'deviceMoved',
+    }
+
+    def setup(self):
+        self.qtcore = pytest.importorskip('{0}.QtCore'.format(
+            self.BINDING_NAME))
+
+    def create_observer(self, monitor):
+        name = self.BINDING_NAME.lower()
+        mod = __import__('pyudev.{0}'.format(name), None, None, [name])
+        self.observer = mod.QUDevMonitorObserver(monitor)
+
+    def connect_signal(self, callback, action=None):
+        if action is None:
+            self.observer.deviceEvent.connect(callback)
+        else:
+            signal = getattr(self.observer, self.ACTION_SIGNAL_MAP[action])
+            signal.connect(callback)
+
+    def create_event_loop(self, self_stop_timeout=5000):
+        self.app = self.qtcore.QCoreApplication.instance()
+        if not self.app:
+            self.app = self.qtcore.QCoreApplication([])
+        self.qtcore.QTimer.singleShot(
+            self_stop_timeout, self.stop_event_loop)
+
+    def start_event_loop(self, start_callback):
+        self.qtcore.QTimer.singleShot(0, start_callback)
+        self.app.exec_()
+
+    def stop_event_loop(self):
+        self.app.quit()
+
+
+class TestPysideObserver(QtObserverTestBase):
+    BINDING_NAME = 'PySide'
+
+
+class TestPyQt4Observer(QtObserverTestBase):
+    BINDING_NAME = 'PyQt4'
+
+
+class TestGlibObserver(ObserverTestBase):
+
+    ACTION_SIGNAL_MAP = {
+        'add': 'device-added',
+        'remove': 'device-removed',
+        'change': 'device-changed',
+        'move': 'device-moved',
+    }
+
+    def setup(self):
+        self.event_sources = []
+        self.glib = pytest.importorskip('glib')
+        # make sure that we also have gobject
+        pytest.importorskip('gobject')
+
+    def teardown(self):
+        for source in self.event_sources:
+            self.glib.source_remove(source)
+
+    def create_observer(self, monitor):
+        from pyudev.glib import GUDevMonitorObserver
+        self.observer = GUDevMonitorObserver(monitor)
+
+    def connect_signal(self, callback, action=None):
+        # drop the sender argument from glib signal connections
+        def _wrapper(obj, *args, **kwargs):
+            return callback(*args, **kwargs)
+        if action is None:
+            self.observer.connect('device-event', _wrapper)
+        else:
+            self.observer.connect(self.ACTION_SIGNAL_MAP[action], _wrapper)
+
+    def create_event_loop(self, self_stop_timeout=5000):
+        self.mainloop = self.glib.MainLoop()
+        self.event_sources.append(
+            self.glib.timeout_add(self_stop_timeout, self.stop_event_loop))
+
+    def start_event_loop(self, start_callback):
+        def _wrapper(*args, **kwargs):
+            start_callback(*args, **kwargs)
+            return False
+        self.event_sources.append(self.glib.timeout_add(0, _wrapper))
+        self.mainloop.run()
+
+    def stop_event_loop(self):
+        self.mainloop.quit()
+        return False
+
+
+class TestWxObserver(ObserverTestBase):
+
+    def setup(self):
+        self.timers = []
+        self.wx = pytest.importorskip('wx')
+
+    def teardown(self):
+        for timer in self.timers:
+            timer.Stop()
+
+    def create_observer(self, monitor):
+        from pyudev import wx
+        self.observer = wx.WxUDevMonitorObserver(monitor)
+        self.action_event_map = {
+                'add': wx.EVT_DEVICE_ADDED,
+                'remove': wx.EVT_DEVICE_REMOVED,
+                'change': wx.EVT_DEVICE_CHANGED,
+                'move': wx.EVT_DEVICE_MOVED
+        }
+
+    def connect_signal(self, callback, action=None):
+        if action is None:
+            from pyudev.wx import EVT_DEVICE_EVENT
+            self.observer.Bind(EVT_DEVICE_EVENT,
+                               lambda e: callback(e.action, e.device))
+        else:
+            self.observer.Bind(self.action_event_map[action],
+                               lambda e: callback(e.device))
+
+    def create_event_loop(self, self_stop_timeout=5000):
+        self.app = self.wx.App(False)
+        self.app.frame = self.wx.Frame(None)
+        timer = self.wx.PyTimer(self.stop_event_loop)
+        timer.Start(self_stop_timeout, True)
+        self.timers.append(timer)
+
+    def start_event_loop(self, start_callback):
+        timer = self.wx.PyTimer(start_callback)
+        timer.Start(0, True)
+        self.timers.append(timer)
+        self.app.MainLoop()
+
+    def stop_event_loop(self):
+        self.app.frame.Close()
