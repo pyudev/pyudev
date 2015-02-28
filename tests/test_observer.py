b34d03bbb39b5f50171236ab5a29af9cecf0cba2
Tobias Gehring <tobias.gehring@fysik.dtu.dk>
2015-02-28 21:25:44 +0100

    added support for pyqt5

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -157,6 +157,10 @@ class TestPyQt4Observer(QtObserverTestBase):
     BINDING_NAME = 'PyQt4'
 
 
+class TestPyQt5Observer(QtObserverTestBase):
+    BINDING_NAME = 'PyQt5'
+
+
 class TestGlibObserver(ObserverTestBase):
 
     def setup(self):
