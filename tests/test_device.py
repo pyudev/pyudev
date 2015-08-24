4dcb7fae522e9026d58344216f1784e90b707cc4
mulhern <amulhern@redhat.com>
2015-08-24 12:12:59 -0400

    If hard-coded device can not be found, skip the test.
    Ideally, for these tests, an existing device should be found.
    
    Hopefully this patch is just temporary.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/test_device.py
+++ tests/test_device.py
@@ -78,8 +78,11 @@ class TestDevice(object):
         assert device == Device.from_path(context, device_data.sys_path)
 
     def test_from_path_strips_leading_slash(self, context):
-        assert Device.from_path(context, 'devices/platform') == \
+        try:
+            assert Device.from_path(context, 'devices/platform') == \
                Device.from_path(context, '/devices/platform')
+        except DeviceNotFoundAtPathError:
+            pytest.skip('device not found')
 
     @with_device_data
     def test_from_sys_path(self, context, device_data):
@@ -503,7 +506,10 @@ class TestDevice(object):
         'operator', ORDERING_OPERATORS,
         ids=[f.__name__ for f in ORDERING_OPERATORS])
     def test_device_ordering(self, context, operator):
-        device = Device.from_path(context, '/devices/platform')
+        try:
+            device = Device.from_path(context, '/devices/platform')
+        except DeviceNotFoundAtPathError:
+            pytest.skip('device not found')
         with pytest.raises(TypeError) as exc_info:
             operator(device, device)
         assert str(exc_info.value) == 'Device not orderable'
