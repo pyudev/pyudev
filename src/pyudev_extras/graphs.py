d4f665e9455597e94c5feba9b8e9469b205a02ff
mulhern <amulhern@redhat.com>
2015-10-12 14:38:52 -0400

    Add ability to transform graphviz graphs for better visualization.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/pyudev_extras/graphs.py
+++ src/pyudev_extras/graphs.py
@@ -32,6 +32,8 @@ from __future__ import unicode_literals
 
 from collections import namedtuple
 
+import os
+
 import networkx as nx
 
 import pyudev
@@ -382,6 +384,94 @@ class GraphNodeDecorations(object):
                 dicts[name][node] = props[name]
 
         return dicts
+
+
+class GraphTransformers(object):
+    """
+    A collection of graph transformers.
+    """
+
+    @staticmethod
+    def copy_attr(attr):
+        """
+        Copy attr object to a dict of keys and values.
+
+        :param attr: the attributes oject
+        :returns: a dict of attributes
+        :rtype: dict
+        """
+        return dict([(k, attr[k]) for k in attr])
+
+    @staticmethod
+    def xform_partition(node):
+        """
+        Transform a partition device so that its label is just the device name.
+        """
+        node.attr['label'] = os.path.basename(node.attr['DEVPATH'])
+
+
+    @classmethod
+    def xform_partitioned(cls, graph, node):
+        """
+        Place a disk node w/edges to partitions in a subgraph
+        that contains it and the partitions.
+
+        :param `AGraph` graph: the graph
+        :param `Node` node: the node
+
+        :returns: the new cluster formed
+        :rtype: `AGraph`
+        """
+        # Find partitions of node
+        partition_edges = [e for e in graph.out_edges(node) if \
+           EdgeTypes.is_type(e, EdgeTypes.PARTITION)]
+        partitions = [e[1] for e in partition_edges]
+
+        cluster = graph.add_subgraph(
+           partitions + [node],
+           name='cluster' + node.name,
+        )
+
+        node.attr['shape'] = "plaintext"
+        for edge in partition_edges:
+            edge.attr['color'] = 'white'
+
+        return cluster
+
+    @classmethod
+    def xform_partitions(cls, graph):
+        """
+        Transformations on partitions and their parents.
+
+        :param `AGraph` graph: the graph
+        """
+        partition_edges = [e for e in graph.iteredges() if \
+               EdgeTypes.is_type(e, EdgeTypes.PARTITION)]
+
+        while partition_edges:
+            cluster = cls.xform_partitioned(graph, partition_edges[0][0])
+            partition_edges = [e for e in partition_edges \
+               if e not in cluster.edges()]
+
+        partitions = (n for n in graph.iternodes() if \
+           NodeTypes.is_type(n, NodeTypes.DEVICE_PATH) and \
+           n.attr['DEVTYPE'] == 'partition')
+
+        for partition in partitions:
+            cls.xform_partition(partition)
+
+    @classmethod
+    def xform(cls, graph):
+        """
+        Transform a networkx multigraph to a graphviz graph.
+
+        :param `DiMultiGraph` graph: the networkx graph
+        :returns: the new graph
+        :rtype: `A_Graph`
+        """
+        res = nx.to_agraph(graph)
+        cls.xform_partitions(graph)
+        return res
 
 
 class DisplayGraph(object):
