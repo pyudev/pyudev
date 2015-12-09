653d9b2fe01729e30dca7a7d75d5626c86bca41c
mulhern <amulhern@redhat.com>
2015-12-09 17:20:28 -0500

    Move udev_version into _util.
    To avoid circular imports.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/pyudev/_util.py
+++ src/pyudev/_util.py
@@ -29,6 +29,11 @@
 from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
 
+try:
+    from subprocess import check_output
+except ImportError:
+    from pyudev._compat import check_output
+
 import os
 import sys
 import stat
@@ -172,3 +177,31 @@ def eintr_retry_call(func, *args, **kwargs):
             if error_code == errno.EINTR:
                 continue
             raise
+
+def udev_version():
+    """
+    Get the version of the underlying udev library.
+
+    udev doesn't use a standard major-minor versioning scheme, but instead
+    labels releases with a single consecutive number.  Consequently, the
+    version number returned by this function is a single integer, and not a
+    tuple (like for instance the interpreter version in
+    :data:`sys.version_info`).
+
+    As libudev itself does not provide a function to query the version number,
+    this function calls the ``udevadm`` utility, so be prepared to catch
+    :exc:`~exceptions.EnvironmentError` and
+    :exc:`~subprocess.CalledProcessError` if you call this function.
+
+    Return the version number as single integer.  Raise
+    :exc:`~exceptions.ValueError`, if the version number retrieved from udev
+    could not be converted to an integer.  Raise
+    :exc:`~exceptions.EnvironmentError`, if ``udevadm`` was not found, or could
+    not be executed.  Raise :exc:`subprocess.CalledProcessError`, if
+    ``udevadm`` returned a non-zero exit code.  On Python 2.7 or newer, the
+    ``output`` attribute of this exception is correctly set.
+
+    .. versionadded:: 0.8
+    """
+    output = ensure_unicode_string(check_output(['udevadm', '--version']))
+    return int(output.strip())
