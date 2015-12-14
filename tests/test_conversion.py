111e9b8e9f53d480544615af4be5b3d5bb8a8d8d
mulhern <amulhern@redhat.com>
2015-12-14 10:09:56 -0500

    Test equivalence of conversion methods with asstring methods.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -30,13 +30,23 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from hypothesis import given
+from hypothesis import strategies
+from hypothesis import Settings
+
 import pytest
 
 from pyudev import ConversionError
+from pyudev import Devices
 from pyudev import SysfsConversion
 from pyudev import SysfsConversions
 from pyudev import UdevConversion
 from pyudev import UdevConversions
+
+from ._constants import _CONTEXT_STRATEGY
+from ._constants import _DEVICE_DATA
+from ._constants import _DEVICES
+from ._constants import non_volatile_attributes
 
 
 class TestUdevConversion(object):
@@ -73,3 +83,167 @@ class TestSysfsConversion(object):
             SysfsConversion.convert(u'so', SysfsConversions.to_int)
         with pytest.raises(ConversionError):
             SysfsConversion.convert(u'true', SysfsConversions.to_bool)
+
+
+class TestUdevEquivalence(object):
+    """
+    Test that converting using conversion methods works like instance methods.
+    """
+    # pylint: disable=too-few-public-methods
+
+    @given(
+       strategies.sampled_from(_DEVICES),
+       settings=Settings(max_examples=30)
+    )
+    def test_int_conversion(self, a_device):
+        """
+        Test conversion to an expected int.
+        """
+        for prop in a_device:
+            try:
+                old = a_device.asint(prop)
+            except KeyError:
+                with pytest.raises(KeyError):
+                    # pylint: disable=pointless-statement
+                    a_device[prop]
+            except ValueError as old_err:
+                with pytest.raises(ConversionError) as new_err:
+                    UdevConversion.convert(
+                       a_device[prop],
+                       UdevConversions.to_int
+                    )
+                    assert new_err.message == old_err
+            else:
+                new = UdevConversion.convert(
+                   a_device[prop],
+                   UdevConversions.to_int
+                )
+                assert old == new
+
+    @given(
+       strategies.sampled_from(_DEVICES),
+       settings=Settings(max_examples=30)
+    )
+    def test_bool_conversion(self, a_device):
+        """
+        Test conversion to an expected bool.
+        """
+        for prop in a_device:
+            try:
+                old = a_device.asbool(prop)
+            except KeyError:
+                with pytest.raises(KeyError):
+                    # pylint: disable=pointless-statement
+                    a_device[prop]
+            except ValueError as old_err:
+                with pytest.raises(ConversionError) as new_err:
+                    UdevConversion.convert(
+                       a_device[prop],
+                       UdevConversions.to_bool
+                    )
+                    assert new_err.message == old_err
+            else:
+                new = UdevConversion.convert(
+                   a_device[prop],
+                   UdevConversions.to_bool
+                )
+                assert old == new
+
+
+class TestSysfsEquivalence(object):
+    """
+    Test that converting using conversion methods works like instance methods.
+    """
+
+    @given(
+       _CONTEXT_STRATEGY,
+       strategies.sampled_from(_DEVICE_DATA),
+       settings=Settings(max_examples=30)
+    )
+    def test_string_conversion(self, a_context, device_datum):
+        """
+        Test conversion to an expected unicode string.
+        """
+        device = Devices.from_path(a_context, device_datum.device_path)
+        for key, _ in non_volatile_attributes(device_datum.attributes):
+            try:
+                old = device.attributes.asstring(key)
+            except KeyError:
+                with pytest.raises(KeyError):
+                    # pylint: disable=pointless-statement
+                    assert device.attributes.get(key) is None
+            except UnicodeDecodeError as old_err:
+                with pytest.raises(ConversionError) as new_err:
+                    SysfsConversion.convert(
+                       device.attributes.get(key),
+                       SysfsConversions.to_unicode
+                    )
+                    assert new_err.message == old_err
+            else:
+                new = SysfsConversion.convert(
+                   device.attributes.get(key),
+                   SysfsConversions.to_unicode
+                )
+                assert old == new
+
+    @given(
+       _CONTEXT_STRATEGY,
+       strategies.sampled_from(_DEVICE_DATA),
+       settings=Settings(max_examples=30)
+    )
+    def test_int_conversion(self, a_context, device_datum):
+        """
+        Test conversion to an expected int.
+        """
+        device = Devices.from_path(a_context, device_datum.device_path)
+        for key, _ in non_volatile_attributes(device_datum.attributes):
+            try:
+                old = device.attributes.asint(key)
+            except KeyError:
+                with pytest.raises(KeyError):
+                    # pylint: disable=pointless-statement
+                    assert device.attributes.get(key) is None
+            except (UnicodeDecodeError, ValueError) as old_err:
+                with pytest.raises(ConversionError) as new_err:
+                    SysfsConversion.convert(
+                       device.attributes.get(key),
+                       SysfsConversions.to_int
+                    )
+                    assert new_err.message == old_err
+            else:
+                new = SysfsConversion.convert(
+                   device.attributes.get(key),
+                   SysfsConversions.to_int
+                )
+                assert old == new
+
+    @given(
+       _CONTEXT_STRATEGY,
+       strategies.sampled_from(_DEVICE_DATA),
+       settings=Settings(max_examples=50)
+    )
+    def test_bool_conversion(self, a_context, device_datum):
+        """
+        Test conversion to an expected bool.
+        """
+        device = Devices.from_path(a_context, device_datum.device_path)
+        for key, _ in non_volatile_attributes(device_datum.attributes):
+            try:
+                old = device.attributes.asbool(key)
+            except KeyError:
+                with pytest.raises(KeyError):
+                    # pylint: disable=pointless-statement
+                    assert device.attributes.get(key) is None
+            except (UnicodeDecodeError, ValueError) as old_err:
+                with pytest.raises(ConversionError) as new_err:
+                    SysfsConversion.convert(
+                       device.attributes.get(key),
+                       SysfsConversions.to_bool
+                    )
+                    assert new_err.message == old_err
+            else:
+                new = SysfsConversion.convert(
+                   device.attributes.get(key),
+                   SysfsConversions.to_bool
+                )
+                assert old == new
