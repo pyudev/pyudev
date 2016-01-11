33105f5ba2646a1ee00f28e0a16cef1d391d4fa2
mulhern <amulhern@redhat.com>
2016-01-11 10:57:08 -0500

    nomatch == True does not get the complement of nomatch == False.
    It omits devices which have no subsystem.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/pyudev/core.py
+++ src/pyudev/core.py
@@ -280,6 +280,9 @@ class Enumerator(object):
         ``False``), the match is inverted:  A device is only included if it is
         *not* part of the given ``subsystem``.
 
+        Note that, if a device has no subsystem, it is not included either
+        with value of ``nomatch`` True or with value of ``nomatch`` False.
+
         Return the instance again.
         """
         match = (self._libudev.udev_enumerate_add_match_subsystem
