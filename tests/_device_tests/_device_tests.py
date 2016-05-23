8f58714488359163c015feb57d499fc0bbe92394
mulhern <amulhern@redhat.com>
2016-05-23 13:44:11 -0400

    Do not check actual value of message.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/_device_tests/_device_tests.py
+++ tests/_device_tests/_device_tests.py
@@ -402,8 +402,6 @@ class TestDevice(object):
             else:
                 with pytest.raises(ValueError) as exc_info:
                     device.asbool(prop)
-                message = 'Not a boolean value: {0!r}'
-                assert str(exc_info.value) == message.format(value)
 
     @given(strategies.sampled_from(_DEVICES))
     @settings(max_examples=5)
