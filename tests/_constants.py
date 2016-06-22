9992fb62f85056b1c0623d34d6b7d449b8d733fb
mulhern <amulhern@redhat.com>
2016-06-22 14:39:44 -0400

    Add another filter for strategies that start with '['.
    That starting with '[' is actually the problem is just a guess.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/_constants.py
+++ tests/_constants.py
@@ -91,11 +91,10 @@ _ATTRIBUTE_STRATEGY = _ATTRIBUTE_STRATEGY.filter(lambda p: p[1] is not None)
 
 if _UDEV_VERSION <= 222:
     _ATTRIBUTE_STRATEGY = \
-       _ATTRIBUTE_STRATEGY.filter(lambda p: not p[1].startswith(b"\\"))
-
-if _UDEV_VERSION <= 222:
-    _ATTRIBUTE_STRATEGY = \
-       _ATTRIBUTE_STRATEGY.filter(lambda p: not p[1][-1:] == b" ")
+       _ATTRIBUTE_STRATEGY.filter(
+          lambda p: not p[1].startswith(b"\\") and not p[1][-1:] == b" " and \
+          not p[1].startswith(b'[')
+       )
 
 # the tags object for a given device
 _TAGS_STRATEGY = _DEVICE_STRATEGY.map(lambda d: d.tags)
