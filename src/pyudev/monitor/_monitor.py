4aa8b32049ab13dab96864a800fbdb6119599cab
mulhern <amulhern@redhat.com>
2016-07-14 09:51:01 -0400

    Tidy up in monitor.py and related files.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/pyudev/monitor/_monitor.py
+++ src/pyudev/monitor/_monitor.py
@@ -353,7 +353,9 @@ class Monitor(object):
             # .poll() takes timeout in milliseconds
             timeout = int(timeout * 1000)
         self.start()
-        if eintr_retry_call(poll.Poll.for_events((self, 'r')).poll, timeout):
+        events = \
+           eintr_retry_call(poll.Poll.for_events((self, 'r')).poll, timeout)
+        if events:
             return self._receive_device()
         else:
             return None
