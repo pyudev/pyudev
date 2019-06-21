740c72dbbb21379b3bbaf486718dd703759d0fc4
Florent Thiéry <florent.thiery@ubicast.eu>
2019-06-21 14:17:23 +0200

    clarify Deprecation warning triggered when testing if a Device exists without comparing against None

--- src/pyudev/device/_device.py
+++ src/pyudev/device/_device.py
@@ -911,7 +911,7 @@ class Device(Mapping):
         """
         import warnings
         warnings.warn(
-            'Will be removed in 1.0. Access properties with Device.properties.',
+            'Will be removed in 1.0. Access the amount of properties with len(Device.properties). If you are testing if the Device exists, compare explicitly against None.',
             DeprecationWarning,
             stacklevel=2)
         return self.properties.__len__()
