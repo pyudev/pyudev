1464c9dc4701605ff0f4094a672b3d173f4cb629
Skruppy <skruppy@onmars.eu>
2020-06-18 01:38:57 +0200

    Enable GLib unit tests

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -146,9 +146,8 @@ class TestPyQt5Observer(QtObserverTestBase):
 class TestGlibObserver(ObserverTestBase):
     def setup(self):
         self.event_sources = []
-        self.glib = pytest.importorskip('glib')
-        # make sure that we also have gobject
-        pytest.importorskip('gobject')
+        pytest.importorskip('gi')
+        self.glib = pytest.importorskip('gi.repository.GLib')
 
     def teardown(self):
         for source in self.event_sources:
